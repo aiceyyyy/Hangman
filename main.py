9fa52847802291b659c70286ac9cb2e89d713843
Jakub Urbański <jakuburbanski2007@gmail.com>
2024-05-25 18:52:36 +0200

    Initialize project

---
+++ main.py
@@ -0,0 +1,40 @@
+import random
+import hangman
+user_lifes = 6
+password = hangman.words[random.randint(0, 63)]
+hangman_index = 0
+password_guess = len(password)
+
+
+def printHangman(index):
+    print(hangman.HANGMANPICS[index])
+    print(" ")
+    # For tests
+    # print(password)
+    # print(" ")
+
+print(" ")
+print("Welcome to hangman game!")
+printHangman(0)
+
+# Asking till 0 lifes
+while user_lifes > 0 and password_guess != 0:
+    user_guess = input("Guess a letter (category animals): ")
+    if user_guess not in password:
+        user_lifes -= 1
+        hangman_index += 1
+        printHangman(hangman_index)
+    else:
+        printHangman(hangman_index)
+        password_guess -= 1
+
+# Lose info
+if user_lifes == 0:
+    print(hangman.HANGMANPICS[-1])
+    print(" ")
+    print("You lost!")
+    print(f"The password was: {password}")
+
+if user_lifes > 0 and password_guess == 0:
+    printHangman(hangman_index)
+    print("You won!")
