8a20d9c5d6e8aeb73ac21365d4637a07cc5751e6
W1ck29 <janek.adrenalin@gmail.com>
2024-05-28 23:00:00 +0100

    gowno

--- hangman.py
+++ hangman.py
@@ -47,7 +47,7 @@ HANGMANPICS = ['''
   |   |
   O   |
  /|\  |
- / \  |
+ / \  | 
       |
 =========''']
 
@@ -55,6 +55,6 @@ words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
          'coyote crow deer dog donkey duck eagle ferret fox frog goat '
          'goose hawk lion lizard llama mole monkey moose mouse mule newt '
          'otter owl panda parrot pigeon python rabbit ram rat raven '
-         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'rhino salmon seal shark sheep skunk sloth jacob si_plasPlas snake spider '
          'stork swan tiger toad trout turkey turtle weasel whale wolf '
          'wombat zebra ').split()
